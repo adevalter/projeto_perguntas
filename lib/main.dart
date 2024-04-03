cafca936afea2044e04739fef7e2f347552936c8
Adevalter <adevalter@hotmail.com>
2024-04-03 13:22:30 -0300

    refatorado codigo para utilizar listas  e map

--- lib/main.dart
+++ lib/main.dart
@@ -5,34 +5,45 @@ import './questao.dart';
 void main() => runApp(new PerguntasApp());
 
 class _PerguntaAppState extends State<PerguntasApp> {
-  var _perguntaSelcionada = 0;
+  var _perguntaSelecionada = 0;
 
   void _responder() {
     setState(() {
-      _perguntaSelcionada++;
+      _perguntaSelecionada++;
     });
 
-    print(_perguntaSelcionada);
+    print(_perguntaSelecionada);
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<String> perguntas = [
-      "Qual é a sua cor favorita?",
-      "Qual é o seu animal favorito"
+    final List<Map<String, Object>> perguntas = [
+      {
+        'texto': "Qual é a sua cor favorita?",
+        'resposta': ['Preto', 'Vermelho', 'Verde', 'Branco']
+      },
+      {
+        'texto': "Qual é o seu animal favorito?",
+        'resposta': ['coelho', 'Cobra', 'Elefante', 'Leão']
+      },
+      {
+        'texto': "Qual é o seu instrutor favorito?",
+        'resposta': ['MAria', 'Joao', 'Leo', 'Pedro']
+      }
     ];
+    List<String> respostas =
+        perguntas[_perguntaSelecionada].cast()['respostas'];
+     
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Perguntas e Respostas"),
+          title: const Text("Perguntas e Respostas"),
           backgroundColor: Color.fromRGBO(65, 105, 225, 100),
         ),
         body: Column(
           children: <Widget>[
-            Questao(perguntas[_perguntaSelcionada]),
-            Resposta("resposta1",_responder),
-            Resposta("resposta2",_responder),
-            Resposta("resposta3",_responder),
+            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
+            ...respostas.map((t) => Resposta(t, _responder)),
           ],
         ),
       ),
